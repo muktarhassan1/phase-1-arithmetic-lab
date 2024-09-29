d84af18745a710057b2654e67bc4a87f1ac9df8c
muktarhassan1 <mukhtarosman1127@gmail.com>
2024-09-29 11:22:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 
 // Write your code here
+const num1 =31;
+const num2 =2;
+
+const multiply = (num1*num2);
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3=22;
+const num4 =6;
+
+const mod = (num3%num4);
+
+const numbers = [10, 15, 20, 5, 3];
+const max = Math.max(...numbers);
+console.log(max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
